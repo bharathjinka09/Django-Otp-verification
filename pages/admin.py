86de73a807e8c388ac5aff823e197f1e112a32f2
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-17 15:58:54 +0530

    Admin layout changed

--- pages/admin.py
+++ pages/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Contact1
 # Register your models here.
-admin.site.register(Contact1)
+
+
+class Contact1Admin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'mobile_number', 'email_id',
+                    'Blood_type', 'City', 'Sample_text', 'date_day', 'gender', 'is_verified')
+
+
+admin.site.register(Contact1, Contact1Admin)
